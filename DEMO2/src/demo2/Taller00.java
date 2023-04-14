ccb53f7a0b1c920556cd23c165bc2909ec8b6649
ivancitodosmil <116390060+ivancitodosmil@users.noreply.github.com>
2023-04-13 21:32:44 -0500

    Taller 00

---
+++ DEMO2/src/demo2/Taller00.java
@@ -0,0 +1,9 @@
+package demo2;
+
+public class Taller00 {
+
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+    
+}
