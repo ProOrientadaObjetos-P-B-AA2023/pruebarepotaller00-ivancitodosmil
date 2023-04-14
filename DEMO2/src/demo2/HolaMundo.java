0317b3ef2a4e776f1644b2bbaceb074f4fe0bb44
ivancitodosmil <116390060+ivancitodosmil@users.noreply.github.com>
2023-04-13 21:15:29 -0500

    Taller 00

---
+++ DEMO2/src/demo2/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo2;
+
+/**
+ *
+ * @author USER
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+}
